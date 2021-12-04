6919a1a8b28db71c0a2a6a37ab403e90ca6cf03f
asaqwa <bursov@web.de>
2021-12-05 00:14:23 +0100

    level 2

--- src/Games/Arkanoid/Arkanoid.java
+++ src/Games/Arkanoid/Arkanoid.java
@@ -1,7 +1,26 @@
 package Games.Arkanoid;
 
 public class Arkanoid {
+    private int width;
+    private int height;
+
     public static void main(String[] args) {
 
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
