f2511e3138447ee654c69594f6f0f7217c3d038d
asaqwa <bursov@web.de>
2021-12-05 11:25:16 +0100

    level 13

--- src/Games/Arkanoid/Ball.java
+++ src/Games/Arkanoid/Ball.java
@@ -1,17 +1,40 @@
 package Games.Arkanoid;
 
 public class Ball extends BaseObject {
-    public Ball(double x, double y, double radius) {
-        super(x, y, radius);
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        isFrozen = true;
     }
 
     @Override
     public void draw(Canvas canvas) {
-
     }
 
     @Override
     public void move() {
+    }
 
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
     }
 }
